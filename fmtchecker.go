b387619f904fc316530c19882bce1c5f667d1327
tkwtx <takai.1201@icloud.com>
2020-11-29 20:23:08 +0900

    [add] import文取得関数追加

--- fmtchecker.go
+++ fmtchecker.go
@@ -1,6 +1,7 @@
 package fmtchecker
 
 import (
+	"fmt"
 	"go/ast"
 
 	"golang.org/x/tools/go/analysis"
@@ -35,30 +36,59 @@ func run(pass *analysis.Pass) (interface{}, error) {
 
 	nodeFilter := []ast.Node{
 		(*ast.ExprStmt)(nil),
+		(*ast.GenDecl)(nil),
 	}
 
 	isp.Preorder(nodeFilter, func(n ast.Node) {
-		expr, ok := n.(*ast.ExprStmt)
-		if !ok {
-			return
+		switch n := n.(type) {
+		// import取得
+		case *ast.GenDecl:
+
+			if result := getImport(n); result != nil {
+				fmt.Println(result)
+			}
+			// func取得
+		case *ast.ExprStmt:
+			call, ok := n.X.(*ast.CallExpr)
+			if !ok {
+				return
+			}
+			selector, ok := call.Fun.(*ast.SelectorExpr)
+			if !ok {
+				return
+			}
+			if targetFunc.checkFunc(selector) {
+				pass.Report(analysis.Diagnostic{
+					Pos:     n.Pos(),
+					Message: "fmt package is used!",
+				})
+			}
 		}
-		call, ok := expr.X.(*ast.CallExpr)
-		if !ok {
-			return
-		}
-		selector, ok := call.Fun.(*ast.SelectorExpr)
-		if !ok {
-			return
-		}
-		if targetFunc.checkFunc(selector) {
-			pass.Report(analysis.Diagnostic{
-				Pos:     n.Pos(),
-				Message: "fmt package is used!",
-			})
-		}
+
 	})
 
 	return nil, nil
+}
+
+func getImport(n *ast.GenDecl) (imports []string) {
+	for _, spec := range n.Specs {
+		importSpec, ok := spec.(*ast.ImportSpec)
+		if !ok {
+			return nil
+		}
+		if importSpec.Name != nil {
+			// Case: use alias import
+			alias := importSpec.Name.Name
+			if alias == "_" {
+				continue
+			}
+			imports = append(imports, alias)
+		} else {
+			// Case: don't use alias import
+			imports = append(imports, importSpec.Path.Value)
+		}
+	}
+	return
 }
 
 func (t *TargetFunc) checkFunc(expr ast.Expr) bool {
