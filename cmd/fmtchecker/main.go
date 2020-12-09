a06179b98b7e20d3a0ab3ac585ee28a905b4beb7
tkwtx <takai.1201@icloud.com>
2020-12-09 18:55:53 +0900

    [fix] package名変更

--- cmd/fmtchecker/main.go
+++ cmd/fmtchecker/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/tkwtx/fmtchecker"
+	"github.com/tkwtx/pkgchecker"
 	"golang.org/x/tools/go/analysis/singlechecker"
 )
 
-func main() { singlechecker.Main(fmtchecker.Analyzer) }
+func main() { singlechecker.Main(pkgchecker.Analyzer) }
--- pkgchecker_test.go
+++ pkgchecker_test.go
@@ -1,16 +1,16 @@
-package fmtchecker_test
+package pkgchecker_test
 
 import (
 	"log"
 	"testing"
 
 	"github.com/gostaticanalysis/testutil"
-	"github.com/tkwtx/fmtchecker"
+	"github.com/tkwtx/pkgchecker"
 	"golang.org/x/tools/go/analysis/analysistest"
 )
 
 func init() {
-	if err := fmtchecker.Analyzer.Flags.Set("name", "foo"); err != nil {
+	if err := pkgchecker.Analyzer.Flags.Set("name", "foo"); err != nil {
 		log.Fatal(err)
 	}
 }
@@ -18,5 +18,5 @@ func init() {
 // TestAnalyzer is a test for Analyzer.
 func TestAnalyzer(t *testing.T) {
 	testdata := testutil.WithModules(t, analysistest.TestData(), nil)
-	analysistest.Run(t, testdata, fmtchecker.Analyzer, "a")
+	analysistest.Run(t, testdata, pkgchecker.Analyzer, "a")
 }
